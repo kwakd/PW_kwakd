78978b99a2e6a43a681e425d6366b22ac64a8564
kwakd <kwakd.oregonstate.edu>
2023-04-17 17:09:31 -0700

    DONE: PW_v1-5
    PWv1.5 DONE
    - still need to make contact sheet
    - upload to web?

--- PW_v1-5/js/musicPlayer.js
+++ PW_v1-5/js/musicPlayer.js
@@ -1,3 +1,8 @@
+function goodLuck()
+{
+    console.log("٩( ᐛ )و");
+}
+
 const musicContainer = document.querySelector('.music-container');
 const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
@@ -5,14 +10,16 @@ const nextBtn = document.querySelector('#next');
 const audio = document.querySelector('#audio');
 const progress = document.querySelector('.progress-bar');
 const progressContainer = document.querySelector('.progress-container');
+const sound = document.querySelector('.sound-bar');
+const soundContainer = document.querySelector('.sound-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#music-cover');
 
 // Song titles
-const songs = ['jojoPose', 'tooCold', 'xxxKirby'];
+const songs = ['1week', 'beMy', 'merryGoRound', '3657', 'weAre'];
 
 // Keep track of songs
-let songIndex = 2;
+let songIndex = Math.floor(Math.random()*5);
 
 // Load Song info
 loadSong(songs[songIndex]);
@@ -22,7 +29,7 @@ function loadSong(song)
 {
     title.innerText = song;
     audio.src = `playground/music_nator/music/${song}.mp3`;
-    cover.src = `playground/music_nator/img/${song}.jpg`;
+    cover.src = `playground/music_nator/img/${song}.png`;
 }
 
 function playSong()
@@ -30,7 +37,7 @@ function playSong()
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fa').classList.remove('fa-play');
     playBtn.querySelector('i.fa').classList.add('fa-pause');
-
+    audio.volume = 0.2;
     audio.play();
 }
 
@@ -87,6 +94,35 @@ function setProgress(e)
     audio.currentTime = (clickX / width) * duration;
 }
 
+function updateSoundProgress(e)
+{
+    // console.log(e.srcElement);
+    const {duration, currentTime} = e.srcElement;
+    // const progressPercent = (currentTime / duration) * 100;
+    // sound.style.width = `${progressPercent}%`
+    const progressPercent = 0;
+    sound.style.width = `${progressPercent}%`;
+}
+
+function setSoundProgress(e)
+{
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const audioVolume = clickX / width;
+    const progressPercent = audioVolume * 100;
+    sound.style.width = `${progressPercent}%`;
+    // console.log(audioVolume*100);
+    if(clickX < 5)
+    {
+        console.log("mute");
+        audio.volume = 0;
+    }
+    else
+    {
+        audio.volume = audioVolume;
+    }
+}
+
 // Event Listeners
 playBtn.addEventListener('click', () =>{
     const isPlaying = musicContainer.classList.contains('play');
@@ -102,11 +138,13 @@ playBtn.addEventListener('click', () =>{
 });
 
 // Change Song Events
+goodLuck();
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
 audio.addEventListener('timeupdate', updateProgress);
 
 progressContainer.addEventListener('click', setProgress);
+soundContainer.addEventListener('click', setSoundProgress);
 
 audio.addEventListener('ended', nextSong);
